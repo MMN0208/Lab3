57664e02169ac140d6d929d147c260d3ce7a6a0b
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2022-10-29 16:10:21 +0700

    Initial commit

---
+++ Traffic_Light/Core/Inc/input_reading.h
@@ -0,0 +1,15 @@
+/*
+ * input_reading.h
+ *
+ *  Created on: Oct 29, 2022
+ *      Author: ADMIN
+ */
+
+#ifndef INC_INPUT_READING_H_
+#define INC_INPUT_READING_H_
+
+void button_reading(void);
+int is_button_pressed(int index);
+int is_button_pressed_1s(int index);
+
+#endif /* INC_INPUT_READING_H_ */
